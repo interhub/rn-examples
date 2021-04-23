cc030766375516fe8b456554c0b2787caaf1beef
stepanturchenko <stepanstepan4@gmail.com>
2021-04-23 14:06:50 +0200

    add work accelerometer

--- examples/AccelerometerLib/screen/Start.tsx
+++ examples/AccelerometerLib/screen/Start.tsx
@@ -1,61 +1,51 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect, useMemo} from 'react'
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native'
 import {Accelerometer} from 'expo-sensors'
 import Animated, {Easing, useAnimatedStyle, useSharedValue, withTiming} from 'react-native-reanimated'
+import SIZE from '../../../src/SIZE'
 
 export default function App() {
-    const [data, setData] = useState<any>({
-        x: 0,
-        y: 0,
-        z: 0,
-    })
     const _slow = () => {
         Accelerometer.setUpdateInterval(1000)
     }
 
     const _fast = () => {
-        Accelerometer.setUpdateInterval(100)
+        Accelerometer.setUpdateInterval(200)
     }
 
     useEffect(() => {
-        const sub = Accelerometer.addListener(accelerometerData => {
-            setData(accelerometerData)
+        const sub = Accelerometer.addListener(({x, y}) => {
+            'worklet'
+            setUpNewPos({x, y})
         })
         return () => {
             sub.remove()
         }
     }, [])
 
-    const {x, y, z} = data
 
     const [posX, posY] = [useSharedValue(0), useSharedValue(0)]
-    const posStyle = useAnimatedStyle(() => ({
-        transform: [{translateY: posY.value}, {translateX: posX.value}]
-    }))
+
     const fixedRound = (num: number) => {
         'worklet'
-        return parseFloat(Number(num || 0).toFixed(2))
+        return parseFloat(Number(num || 0).toFixed(3))
     }
     const setUpNewPos = ({x, y}: { x: number, y: number }) => {
         'worklet'
-        const COF = 50
-        const newX = fixedRound(x * COF)
-        const newY = fixedRound(y * COF)
-        posX.value = withTiming(-newX, {duration: 200, easing: Easing.linear})
-        posY.value = withTiming(newY, {duration: 200, easing: Easing.linear})
+        const SPEED = 200
+        const newX = fixedRound(x * SPEED)
+        const newY = fixedRound(y * SPEED)
+        const config: Animated.WithTimingConfig = {duration: 500, easing: Easing.linear}
+        posX.value = withTiming(-newX, config)
+        posY.value = withTiming(newY, config)
     }
 
-    useEffect(() => {
-        setUpNewPos({x, y})
-    }, [x, y])
 
+    const POINT_MATCH = 30
 
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Accelerometer: (in Gs where 1 G = 9.81 m s^-2)</Text>
-            <Text style={styles.text}>
-                x: {round(x)} y: {round(y)} z: {round(z)}
-            </Text>
             <View style={styles.buttonContainer}>
                 <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
                     <Text>Slow</Text>
@@ -64,17 +54,41 @@ export default function App() {
                     <Text>Fast</Text>
                 </TouchableOpacity>
             </View>
-            <Animated.View style={[{
-                width: 40,
-                height: 40,
-                backgroundColor: 'red',
-                position: 'absolute',
-                left: 100,
-                top: 200
-            }, posStyle]}/>
+            {new Array(POINT_MATCH).fill(1).map((_, index) => {
+                return <BoxItem posX={posX} posY={posY} key={index}/>
+            })}
         </View>
     )
 }
+
+const BoxItem = React.memo(({posY, posX}: { posY: Animated.SharedValue<any>, posX: Animated.SharedValue<any> }) => {
+    const POINT_SIZE = 20
+
+    const animStyle = useAnimatedStyle(() => ({
+        transform: [{translateY: posY.value}, {translateX: posX.value}]
+    }))
+
+    const getRandomCoord = () => {
+        'worklet'
+        const diff = (POINT_SIZE / 2)
+        const {width, height} = SIZE
+        const x = Math.random() * width - diff
+        const y = Math.random() * height - diff
+        return {x, y}
+    }
+
+    const {x: left, y: top} = useMemo(() => getRandomCoord(), [])
+
+    return <Animated.View
+        style={[{
+            width: POINT_SIZE,
+            height: POINT_SIZE,
+            backgroundColor: 'red',
+            position: 'absolute',
+            left,
+            top
+        }, animStyle]}/>
+}, () => true)
 
 function round(n: any) {
     if (!n) {
