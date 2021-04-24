0a65976aa1916789d1a153bc45b375e67504eaf4
stepanturchenko <stepanstepan4@gmail.com>
2021-04-24 14:57:47 +0200

    create context provider

--- examples/AccelerometerLib/screen/Start.tsx
+++ examples/AccelerometerLib/screen/Start.tsx
@@ -1,72 +1,28 @@
-import React, {useEffect, useMemo} from 'react'
+import React, {useContext, useEffect, useMemo} from 'react'
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native'
 import {Accelerometer} from 'expo-sensors'
 import Animated, {Easing, useAnimatedStyle, useSharedValue, withTiming} from 'react-native-reanimated'
 import SIZE from '../../../src/SIZE'
 
 export default function App() {
-    const _slow = () => {
-        Accelerometer.setUpdateInterval(1000)
-    }
-
-    const _fast = () => {
-        Accelerometer.setUpdateInterval(200)
-    }
-
-    useEffect(() => {
-        const sub = Accelerometer.addListener(({x, y}) => {
-            'worklet'
-            setUpNewPos({x, y})
-        })
-        return () => {
-            sub.remove()
-        }
-    }, [])
-
-
-    const [posX, posY] = [useSharedValue(0), useSharedValue(0)]
-
-    const fixedRound = (num: number) => {
-        'worklet'
-        return parseFloat(Number(num || 0).toFixed(3))
-    }
-    const setUpNewPos = ({x, y}: { x: number, y: number }) => {
-        'worklet'
-        const SPEED = 200
-        const newX = fixedRound(x * SPEED)
-        const newY = fixedRound(y * SPEED)
-        const config: Animated.WithTimingConfig = {duration: 500, easing: Easing.linear}
-        posX.value = withTiming(-newX, config)
-        posY.value = withTiming(newY, config)
-    }
-
 
     const POINT_MATCH = 30
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Accelerometer: (in Gs where 1 G = 9.81 m s^-2)</Text>
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
-                    <Text>Slow</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={_fast} style={styles.button}>
-                    <Text>Fast</Text>
-                </TouchableOpacity>
+        <ParallaxProvider>
+            <View style={styles.container}>
+                <Text style={styles.text}>Accelerometer: (in Gs where 1 G = 9.81 m s^-2)</Text>
+                {new Array(POINT_MATCH).fill(1).map((_, index) => {
+                    return <BoxItem key={index}/>
+                })}
             </View>
-            {new Array(POINT_MATCH).fill(1).map((_, index) => {
-                return <BoxItem posX={posX} posY={posY} key={index}/>
-            })}
-        </View>
+        </ParallaxProvider>
     )
 }
 
-const BoxItem = React.memo(({posY, posX}: { posY: Animated.SharedValue<any>, posX: Animated.SharedValue<any> }) => {
+const BoxItem = React.memo(() => {
     const POINT_SIZE = 20
 
-    const animStyle = useAnimatedStyle(() => ({
-        transform: [{translateY: posY.value}, {translateX: posX.value}]
-    }))
 
     const getRandomCoord = () => {
         'worklet'
@@ -79,6 +35,8 @@ const BoxItem = React.memo(({posY, posX}: { posY: Animated.SharedValue<any>, pos
 
     const {x: left, y: top} = useMemo(() => getRandomCoord(), [])
 
+    const {animStyle} = useParallax()
+
     return <Animated.View
         style={[{
             width: POINT_SIZE,
@@ -90,12 +48,56 @@ const BoxItem = React.memo(({posY, posX}: { posY: Animated.SharedValue<any>, pos
         }, animStyle]}/>
 }, () => true)
 
-function round(n: any) {
-    if (!n) {
-        return 0
+const AccelerometerContext = React.createContext<{ posX: Animated.SharedValue<number>, posY: Animated.SharedValue<number> }>({} as any)
+
+const ParallaxProvider = ({children}: { children: React.ReactNode }) => {
+
+    const PERIOD = 200
+
+    const fixedRound = (num: number) => {
+        'worklet'
+        return parseFloat(Number(num || 0).toFixed(3))
     }
-    return Math.floor(n * 100) / 100
+
+    const setUpNewPos = ({x, y}: { x: number, y: number }) => {
+        'worklet'
+        const SPEED = 100
+        const newX = fixedRound(x * SPEED)
+        const newY = fixedRound(y * SPEED)
+        const config: Animated.WithTimingConfig = {duration: PERIOD, easing: Easing.linear}
+        posX.value = withTiming(-newX, config)
+        posY.value = withTiming(newY, config)
+    }
+
+    useEffect(() => {
+        Accelerometer.setUpdateInterval(PERIOD)
+
+        const sub = Accelerometer.addListener(({x, y}) => {
+            'worklet'
+            setUpNewPos({x, y})
+        })
+        return () => {
+            sub.remove()
+        }
+    }, [])
+
+    const [posX, posY] = [useSharedValue(0), useSharedValue(0)]
+
+    return <AccelerometerContext.Provider value={{posX, posY}}>
+        {children}
+    </AccelerometerContext.Provider>
 }
+
+const useParallax = () => {
+    const {posY, posX} = useContext(AccelerometerContext)
+
+    const animStyle = useAnimatedStyle(() => ({
+        transform: [{translateY: posY.value}, {translateX: posX.value}]
+    }))
+
+    return {animStyle}
+}
+
 
 const styles = StyleSheet.create({
     container: {
@@ -105,22 +107,5 @@ const styles = StyleSheet.create({
     },
     text: {
         textAlign: 'center',
-    },
-    buttonContainer: {
-        flexDirection: 'row',
-        alignItems: 'stretch',
-        marginTop: 15,
-    },
-    button: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#eee',
-        padding: 10,
-    },
-    middleButton: {
-        borderLeftWidth: 1,
-        borderRightWidth: 1,
-        borderColor: '#ccc',
     },
 })
