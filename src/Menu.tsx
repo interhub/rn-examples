268cb7e505a3d66d36d7ef0f1102e8baaa116295
stepanturchenko <stepanstepan4@gmail.com>
2021-04-27 13:45:00 +0200

    add fix menu

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,34 +1,40 @@
-import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { Button, Text, View } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { SCREEN_NAME } from './SCREEN_NAME';
+import {useNavigation} from '@react-navigation/native'
+import React from 'react'
+import {Button, Text, View} from 'react-native'
+import {FlatList} from 'react-native-gesture-handler'
+import {useSafeAreaInsets} from 'react-native-safe-area-context'
+import {SCREEN_NAME} from './SCREEN_NAME'
+import ButtonCustom from '../components/ButtonCustom'
 
 const Menu = () => {
 
-    const { top } = useSafeAreaInsets()
+    const {top, bottom} = useSafeAreaInsets()
     const SCREENS = Object.values(SCREEN_NAME)
 
     return (
         <FlatList
-            style={{ flex: 1, paddingTop: top }}
+            contentContainerStyle={{paddingTop: top, paddingBottom: bottom + 10}}
+            style={{flex: 1}}
             data={SCREENS}
             keyExtractor={(item, index) => index.toString()}
-            renderItem={({ item, index }) => {
-                return <NavigateButton screenName={item} />
+            renderItem={({item, index}) => {
+                return <NavigateButton screenName={item}/>
             }}
         />
-    );
+    )
 }
 
 
-const NavigateButton = ({ screenName }: { screenName: SCREEN_NAME }) => {
-    const { navigate } = useNavigation()
+const NavigateButton = ({screenName}: { screenName: SCREEN_NAME }) => {
+    const {navigate} = useNavigation()
 
     const onPressHandler = () => navigate(screenName)
 
-    return <Button title={String(screenName)} onPress={onPressHandler} />
+    return <View style={{paddingHorizontal: 10, paddingTop: 10}}>
+        <ButtonCustom onPress={onPressHandler}>
+            {String(screenName)}
+        </ButtonCustom>
+    </View>
 }
 
 export default Menu
