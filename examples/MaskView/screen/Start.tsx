af25ec6bd1767573a279f5ab71dde9c7d99d134c
stepanturchenko <stepanstepan4@gmail.com>
2021-04-29 11:39:44 +0200

    add animating resizing

--- examples/MaskView/screen/Start.tsx
+++ examples/MaskView/screen/Start.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react'
-import {Image} from 'react-native'
+import {Image, SafeAreaView} from 'react-native'
 import MaskedView from '@react-native-masked-view/masked-view'
 import Animated, {useAnimatedStyle, useSharedValue, withRepeat, withSequence, withTiming} from 'react-native-reanimated'
 import SIZE from '../../../src/SIZE'
@@ -8,7 +8,7 @@ export default function () {
     const posY = useSharedValue(0)
     const posX = useSharedValue(0)
     useEffect(() => {
-        posY.value = withRepeat(withSequence(withTiming(SIZE.height / 3, {duration: 3000}), withTiming(0, {duration: 3000})), -1, true)
+        posY.value = withRepeat(withSequence(withTiming(SIZE.height / 2.5, {duration: 3000}), withTiming(0, {duration: 3000})), -1, true)
     }, [])
     const textStyle = useAnimatedStyle(() => ({
         transform: [{translateY: posY.value}, {translateX: posX.value}]
@@ -16,23 +16,25 @@ export default function () {
 
 
     return (
-        <MaskedView
-            style={{flex: 1}}
-            maskElement={
-                <Animated.Text
-                    numberOfLines={3}
-                    style={[{
-                        fontSize: SIZE.width / 4,
-                        color: 'black',
-                        fontWeight: 'bold',
-                        textAlign: 'center'
-                    }, textStyle]}
-                >
-                    Basic Mask Example
-                </Animated.Text>
-            }
-        >
-            <Image source={require('../img/bg.jpg')} style={{width: '100%', height: '100%'}}/>
-        </MaskedView>
+        <SafeAreaView style={{flex: 1}}>
+            <MaskedView
+                style={{flex: 1}}
+                maskElement={
+                    <Animated.Text
+                        numberOfLines={3}
+                        style={[{
+                            fontSize: SIZE.width / 4,
+                            color: 'black',
+                            fontWeight: 'bold',
+                            textAlign: 'center'
+                        }, textStyle]}
+                    >
+                        Basic Mask Example
+                    </Animated.Text>
+                }
+            >
+                <Image source={require('../img/bg.jpg')} style={{width: '100%', height: '100%'}}/>
+            </MaskedView>
+        </SafeAreaView>
     )
 }
