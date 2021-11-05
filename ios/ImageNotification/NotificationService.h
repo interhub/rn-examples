718ac9ce00fec2ae21f3cd836e763817771e0859
stepanturchenko <stepanstepan4@gmail.com>
2021-11-05 23:22:05 +0200

    create ios notification images service

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Macbook on 05.11.2021.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
