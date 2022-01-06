534af30129482f76f806c08abe67365b207aeb9a
stepanturchenko <stepanstepan4@gmail.com>
2022-01-06 08:17:20 +0200

    save style props circle

--- examples/CircleProgress/screen/Start.tsx
+++ examples/CircleProgress/screen/Start.tsx
@@ -1,24 +1,22 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useLayoutEffect, useMemo, useRef, useState} from 'react'
 import {ScrollView, StyleSheet, Text, View} from 'react-native'
 import Animated, {useAnimatedProps, useSharedValue, withTiming} from 'react-native-reanimated'
 import Svg, {Circle, Rect} from 'react-native-svg'
 
 //TODO using https://codepen.io/JMChristensen/pen/Ablch !!
 
-const HEADER_SIZE = 100
-
 const CircleAnimate = Animated.createAnimatedComponent(Circle)
 
 export default function () {
-    const SIZE = 300
+    const BORDER_COLOR = '#4649ad'
+    const FILL_COLOR = '#d19d60'
+    const SIZE_C = 300
     const BORDER_WIDTH = 20
     const animValue = useSharedValue(0)
-    const progressPercent = 90
-    const R = (SIZE / 2) - BORDER_WIDTH
+    const progressPercent = 20
+    const R = (SIZE_C / 2) - BORDER_WIDTH
     const LEN = Math.PI * (R * 2)
     const SDO = ((100 - progressPercent) / 100) * LEN
-    // if (val < 0) { val = 0;}
-    // if (val > 100) { val = 100;}
     useEffect(() => {
         setTimeout(() => {
             animValue.value = withTiming(SDO)
@@ -32,19 +30,26 @@ export default function () {
 
     return (
         <View style={styles.container}>
-            <Svg height={SIZE} width={SIZE} viewBox={`0 0 ${SIZE} ${SIZE}`}>
+            <Svg height={SIZE_C} width={SIZE_C} viewBox={`0 0 ${SIZE_C} ${SIZE_C}`}>
                 <CircleAnimate
-                    cx={SIZE / 2}
-                    cy={SIZE / 2}
-                    r={(SIZE / 2) - BORDER_WIDTH}
-                    stroke="blue"
+                    cx={SIZE_C / 2}
+                    cy={SIZE_C / 2}
+                    r={(SIZE_C / 2) - BORDER_WIDTH}
+                    stroke={BORDER_COLOR}
                     strokeWidth={BORDER_WIDTH}
-                    fill="green"
+                    fill={FILL_COLOR}
                     strokeLinecap={'round'}
                     strokeDasharray={LEN}
                     animatedProps={AProps}
                 />
             </Svg>
+            {/*CENTER CHILD CONTAINER*/}
+            <View style={styles.centerChild}>
+                {/*PLACE TO APPLY CHILD INSTEAD*/}
+                <View>
+                    <Text style={{fontSize: 24, color: '#1a1a1a'}}>Progress is {progressPercent}%</Text>
+                </View>
+            </View>
         </View>
     )
 }
@@ -52,20 +57,13 @@ export default function () {
 const styles = StyleSheet.create({
     container: {
         // flex: 1,
-        // backgroundColor: '#cc7575',
-        alignItems:'center'
+        backgroundColor: '#ffa83c',
+        alignItems: 'center'
     },
-    header: {
-        height: HEADER_SIZE,
-        backgroundColor: 'blue',
-        position: 'absolute',
-        width: '100%',
-        zIndex: 10,
-    },
-    title: {
-        textAlign: 'center',
-        marginTop: 'auto',
-        color: '#FFF',
-        fontSize: 20,
-    },
+    centerChild: {
+        ...StyleSheet.absoluteFillObject,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+
 })
