8bc129a161b1c53bd162815416a6989f54d12774
stepanturchenko <stepanstepan4@gmail.com>
2022-01-10 19:13:02 +0200

    add new some params for handle support on device

--- examples/FaceId/screen/Start.tsx
+++ examples/FaceId/screen/Start.tsx
@@ -8,23 +8,24 @@ import Message from '../../../src/config/Message'
  * to work this module you have to set up NSFaceIDUsageDescription Value in Info.plist file
  */
 const showResMessage = (isSuccess: boolean) => Message(isSuccess ? 'Успешно ✅' : 'Не успешно 🤷‍♂️')
-const localAuthDetect = async (): Promise<boolean> => {
+const localAuthDetect = async (): Promise<{ isSuccess: boolean; isSupport: boolean; isActive: boolean, isError?: boolean }> => {
     try {
         const isExistHardware = await LocalAuthentication.hasHardwareAsync()
+        if (!isExistHardware) return {isSuccess: false, isActive: false, isSupport: false}
         const isEnrolledAnyAuth = await LocalAuthentication.isEnrolledAsync()
-        if (!isExistHardware || !isEnrolledAnyAuth) return false
+        if (!isEnrolledAnyAuth) return {isSuccess: false, isSupport: true, isActive: false}
         const {success} = await LocalAuthentication.authenticateAsync()
-        return success
+        return {isSuccess: success, isActive: true, isSupport: true}
     } catch (e) {
         console.log(e)
-        return false
+        return {isSuccess: false, isSupport: true, isActive: true, isError: true}
     }
 }
 
 const FaceId = () => {
     return (
         <View style={styles.container}>
-            <ButtonCustom m={10} onPress={() => localAuthDetect().then((res) => showResMessage(res))}>
+            <ButtonCustom m={10} onPress={() => localAuthDetect().then((res) => showResMessage(res.isSuccess))}>
                 Login Face Id
             </ButtonCustom>
         </View>
