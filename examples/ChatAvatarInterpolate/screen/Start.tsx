938dbb232cb12151eb358d0b345b7371cfbfeea0
stepanturchenko <stepanstepan4@gmail.com>
2022-01-19 01:53:40 +0200

    add hot reload fix to example scroll avatars list

--- examples/ChatAvatarInterpolate/screen/Start.tsx
+++ examples/ChatAvatarInterpolate/screen/Start.tsx
@@ -50,17 +50,20 @@ const MessageBlock = (props: MessageType & { scrollAnimateValue: Animated.Value 
     const {bottom: bottomInsert} = useSafeAreaInsets()
 
     const [interPosition, setInterPositions] = useState({
-        startBottomPosition: 1,
-        endTopPosition: 2
+        startBottomPosition: 0,
+        endTopPosition: 0
     })
+
+    //hot reload fix to except crash after hot update
+    const [animValRef] = useAnimatedLatestValueRef(scrollAnimateValue, 0)
 
     const [isEndAnim, setIsEndAnim] = useState(false)
     useEffect(() => {
         if (!isEndAnim) return
-        if (!navigation.isFocused()) return
         return messageBoxRef?.current?.measure((x, y, w, h, pX, pY) => {
+            const scrollPosition = animValRef.current
             //Top avatar screen position depends on pY scroll and h (to first item should be 0)
-            const startBottomPosition = Math.round(SIZE.height - (pY + h) - bottomInsert) // pY - (SIZE.height - headerHeight)
+            const startBottomPosition = Math.round((SIZE.height - (pY + h) - bottomInsert) + scrollPosition) // pY - (SIZE.height - headerHeight)
             const endTopPosition = Math.round((startBottomPosition + h) - (AVATAR_ICON_SIZE)) // pY - (SIZE.height - headerHeight)
             setInterPositions({endTopPosition, startBottomPosition})
             // console.log({pY, startBottomPosition, endTopPosition, h, username, heightScreen: SIZE.height, bottomInsert})
@@ -106,11 +109,46 @@ const MessageBlock = (props: MessageType & { scrollAnimateValue: Animated.Value 
     </View>
 }
 
+/**
+ * Since there's no (official) way to read an Animated.Value synchronously this is the best solution I could come up with
+ * to have access to an up-to-date copy of the latest value without sacrificing performance.
+ *
+ * @param animatedValue the Animated.Value to track
+ * @param initial Optional initial value if you know it to initialize the latest value ref before the animated value listener fires for the first time
+ *
+ * returns a ref with the latest value of the Animated.Value and a boolean ref indicating if a value has been received yet
+ */
+const useAnimatedLatestValueRef = (animatedValue: Animated.Value, initial?: number) => {
+    //If we're given an initial value then we can pretend we've received a value from the listener already
+    const latestValueRef = useRef(initial ?? 0)
+    const initialized = useRef(typeof initial == 'number')
+
+    useLayoutEffect(() => {
+        const id = animatedValue.addListener((v) => {
+            //Store the latest animated value
+            latestValueRef.current = v.value
+            //Indicate that we've recieved a value
+            initialized.current = true
+        })
+
+        //Return a deregister function to clean up
+        return () => animatedValue.removeListener(id)
+
+        //Note that the behavior here isn't 100% correct if the animatedValue changes -- the returned ref
+        //may refer to the previous animatedValue's latest value until the new listener returns a value
+    }, [animatedValue])
+
+
+    return [latestValueRef, initialized] as const
+}
+
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#111a32',
         paddingHorizontal: MESSAGE_BETWEEN_MARGIN,
+
     },
     messageBlock: {
         flexDirection: 'row',
@@ -119,15 +157,15 @@ const styles = StyleSheet.create({
         width: AVATAR_ICON_SIZE,
         height: AVATAR_ICON_SIZE,
         borderRadius: AVATAR_ICON_SIZE / 2,
-        backgroundColor:'gray'
+        backgroundColor: 'gray'
     },
     textBox: {
         padding: 10,
         backgroundColor: 'rgba(12,77,119,0.73)',
         flex: 1,
         borderRadius: 10,
-        marginTop: MESSAGE_BETWEEN_MARGIN,
-        marginLeft: MESSAGE_BETWEEN_MARGIN
+        marginLeft: MESSAGE_BETWEEN_MARGIN,
+        marginTop: MESSAGE_BETWEEN_MARGIN,//can move to messageBlock to exclude icon near touching
     },
     avatarBox: {
         justifyContent: 'flex-end'
