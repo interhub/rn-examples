f8898dc0e91821cfebe5a0bd86d010983576c509
stepanturchenko <stepanstepan4@gmail.com>
2022-01-30 20:38:20 +0200

    create works native ui components for android and ios!

--- examples/NativeModule/screen/Start.tsx
+++ examples/NativeModule/screen/Start.tsx
@@ -1,54 +1,57 @@
-import React, {useEffect, useRef} from 'react'
-import {Animated, findNodeHandle, PixelRatio, ScrollView, StyleSheet, Text, UIManager, View} from 'react-native'
-import { requireNativeComponent } from 'react-native';
+import React, {useEffect, useRef, useState} from 'react'
+import {findNodeHandle, PixelRatio, requireNativeComponent, StyleSheet, UIManager, View} from 'react-native'
+import IS_IOS from '../../../src/config/IS_IOS'
 
 export const MyViewManager = requireNativeComponent(
-    'MyViewManager'
-);
+    IS_IOS ? 'RNTMap' : 'MyViewManager'
+)
 
-const createFragment = (viewId:any) =>
+const createFragment = (viewId: any) =>
     UIManager.dispatchViewManagerCommand(
         viewId,
         // we are calling the 'create' command
         //@ts-ignore
         UIManager.MyViewManager.Commands.create.toString(),
         [viewId]
-    );
+    )
 
 export const MyView = () => {
-  const ref = useRef(null);
+    const ref = useRef(null)
+    const [isReged, setIsReged] = useState(false)
+    useEffect(() => {
+        if (isReged || IS_IOS) return
+        const viewId = findNodeHandle(ref.current)
+        createFragment(viewId)
+        setIsReged(true)
+    }, [])
 
-  useEffect(() => {
-    const viewId = findNodeHandle(ref.current);
-    createFragment(viewId);
-  }, []);
-
-  return (
-      <MyViewManager
-          style={{
-            // converts dpi to px, provide desired height
-            height: PixelRatio.getPixelSizeForLayoutSize(200),
-            // converts dpi to px, provide desired width
-            width: PixelRatio.getPixelSizeForLayoutSize(200)
-          }}
-          ref={ref}
-      />
-  );
-};
+    return (
+        <MyViewManager
+            //@ts-ignore
+            style={{
+                // converts dpi to px, provide desired height
+                height: PixelRatio.getPixelSizeForLayoutSize(200),
+                // converts dpi to px, provide desired width
+                width: PixelRatio.getPixelSizeForLayoutSize(200)
+            }}
+            ref={ref}
+        />
+    )
+}
 
 
 export default function () {
 
-  return (
-    <View style={styles.container}>
-      <MyView/>
-    </View>
-  )
+    return (
+        <View style={styles.container}>
+            <MyView/>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#d1d1d1',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#d1d1d1',
+    },
 })
