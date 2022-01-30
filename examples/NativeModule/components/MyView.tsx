a88245fb2a3ca9dd59fb96bf7e09d408c725a3da
stepanturchenko <stepanstepan4@gmail.com>
2022-01-30 20:41:55 +0200

    add fix native view hot reload error

---
+++ examples/NativeModule/components/MyView.tsx
@@ -0,0 +1,42 @@
+import React, {useEffect, useRef, useState} from 'react'
+import {findNodeHandle, PixelRatio, requireNativeComponent, StyleSheet, UIManager, View} from 'react-native'
+import IS_IOS from '../../../src/config/IS_IOS'
+
+export const MyViewManager = requireNativeComponent(
+    IS_IOS ? 'RNTMap' : 'MyViewManager'
+)
+
+const createFragment = (viewId: any) =>
+    UIManager.dispatchViewManagerCommand(
+        viewId,
+        // we are calling the 'create' command
+        //@ts-ignore
+        UIManager.MyViewManager.Commands.create.toString(),
+        [viewId]
+    )
+
+const MyView = () => {
+    const ref = useRef(null)
+    const [isReged, setIsReged] = useState(false)
+    useEffect(() => {
+        if (isReged || IS_IOS) return
+        const viewId = findNodeHandle(ref.current)
+        createFragment(viewId)
+        setIsReged(true)
+    }, [])
+
+    return (
+        <MyViewManager
+            //@ts-ignore
+            style={{
+                // converts dpi to px, provide desired height
+                height: PixelRatio.getPixelSizeForLayoutSize(200),
+                // converts dpi to px, provide desired width
+                width: PixelRatio.getPixelSizeForLayoutSize(200)
+            }}
+            ref={ref}
+        />
+    )
+}
+
+export default MyView
--- examples/NativeModule/screen/Start.tsx
+++ examples/NativeModule/screen/Start.tsx
@@ -1,44 +1,6 @@
-import React, {useEffect, useRef, useState} from 'react'
-import {findNodeHandle, PixelRatio, requireNativeComponent, StyleSheet, UIManager, View} from 'react-native'
-import IS_IOS from '../../../src/config/IS_IOS'
-
-export const MyViewManager = requireNativeComponent(
-    IS_IOS ? 'RNTMap' : 'MyViewManager'
-)
-
-const createFragment = (viewId: any) =>
-    UIManager.dispatchViewManagerCommand(
-        viewId,
-        // we are calling the 'create' command
-        //@ts-ignore
-        UIManager.MyViewManager.Commands.create.toString(),
-        [viewId]
-    )
-
-export const MyView = () => {
-    const ref = useRef(null)
-    const [isReged, setIsReged] = useState(false)
-    useEffect(() => {
-        if (isReged || IS_IOS) return
-        const viewId = findNodeHandle(ref.current)
-        createFragment(viewId)
-        setIsReged(true)
-    }, [])
-
-    return (
-        <MyViewManager
-            //@ts-ignore
-            style={{
-                // converts dpi to px, provide desired height
-                height: PixelRatio.getPixelSizeForLayoutSize(200),
-                // converts dpi to px, provide desired width
-                width: PixelRatio.getPixelSizeForLayoutSize(200)
-            }}
-            ref={ref}
-        />
-    )
-}
-
+import {StyleSheet, View} from 'react-native'
+import React from 'react'
+import MyView from '../components/MyView'
 
 export default function () {
 
@@ -52,6 +14,6 @@ export default function () {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#d1d1d1',
+        backgroundColor: '#b2b2b2',
     },
 })
