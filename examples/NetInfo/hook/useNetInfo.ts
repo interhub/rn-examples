ff6e401e65e35ddbf0164cbe21a59b46ee16361c
stepanturchenko <stepanstepan4@gmail.com>
2022-02-01 12:11:36 +0200

    add create net info hook to using connection data with expo 2

--- examples/NetInfo/hook/useNetInfo.ts
+++ examples/NetInfo/hook/useNetInfo.ts
@@ -3,7 +3,6 @@ import NetInfo from '@react-native-community/netinfo'
 
 const useNetInfo = () => {
     const [netInfoState, setNetInfoState] = useState({isConnected: true, isWork: true, type: '', ip: ''})
-
     useEffect(() => {
         return NetInfo.addEventListener((state) => {
             //@ts-ignore
