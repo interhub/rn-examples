ccca3d310efe12b47f851096e14fd4218452815d
stepanturchenko <stepanstepan4@gmail.com>
2022-02-09 18:31:32 +0200

    add fix todo keyboard 2

--- components/KeyboardWrapper.tsx
+++ components/KeyboardWrapper.tsx
@@ -3,11 +3,7 @@ import {KeyboardAvoidingView} from 'react-native'
 
 import IS_IOS from '../src/config/IS_IOS'
 
-interface ProviderKeyboardBarProps {
-  children: React.ReactNode
-}
-
-const KeyboardWrapper = ({children}: ProviderKeyboardBarProps) => {
+const KeyboardWrapper: React.FC = ({children}) => {
   return (
     <KeyboardAvoidingView pointerEvents={'box-none'} style={[{flex: 1}]} behavior={IS_IOS ? 'padding' : undefined}>
       {children}
